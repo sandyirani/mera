9d3f35c373997214d9b22254525acac224cbc443
sandyirani <31449386+sandyirani@users.noreply.github.com>
2018-07-18 14:01:48 -0700

    commit

--- wave.jl
+++ wave.jl
@@ -1,1 +1,46 @@
-dfdsdfds
+using PolynomialRoots
+
+L = 2
+K = 3
+tau = 1/2
+d = ones(L+1)
+for j = 2:L+1
+  d[j] = d[j-1]*(L-j+2)*(L-j+2-tau)/(j-1)/(j-1+tau)
+end
+s1 = [binomial(2*K,j) for j = 0:2*K]
+s2 = conv(d,[d[j] for j = length(d):-1:1])
+s = conv(s1,s2)
+M = K+L
+C = zeros(4*M-1,2*M-1)
+for j = 1:4*M-1
+  last = min(j,2*M-1)
+  first = max(1,j-2*M)
+  ffirst = max(1,j-2*M+2)
+  flast = last-first+ffirst
+  C[j,first:last] = s[flast:-1:ffirst]
+end
+@show(C)
+@show(s)
+C = C[2:2:size(C,1),:]
+b = zeros(2*M-1)
+b[M] = 1
+r = C\b
+
+function applyPoly(p,x)
+  n = length(p)
+  f = ones(n)
+  for j = 2:n
+    f[j] = f[j-1]*x
+  end
+  return(f'*p)
+end
+
+function testPoly(p,low,high,eps)
+
+  for x = low:eps:high
+    val = applyPoly(p,x)
+    if (val < 0)
+      println(val)
+    end
+  end
+end
