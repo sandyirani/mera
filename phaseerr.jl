746c60cb47c22fd5eea41c5687f818944f721186
sandyirani <31449386+sandyirani@users.noreply.github.com>
2018-07-31 08:47:02 -0700

    Commit

--- phaseerr.jl
+++ phaseerr.jl
@@ -1,7 +1,7 @@
-L = 5
+L = 2
 tau = 1/2
 taup = -(L-tau)/2
-n = 10
+n = 20
 
 function makeD()
     d = ones(L+1)
@@ -33,9 +33,10 @@ function makeQ(d)
         a[j] = -1 * a[j-1] * taup^2 / (2*j) / (2*j-1)
     end
     b = ones(n)
-    for j = 1:n
-        b[j] = sum(d2) * (-1)^(j-1) / factorial(2*j-2)
+    for j = 0:n-1
+        b[j+1] = sum(d2) * (-1)^(j)
         d2 = d2 .* sq
+        d2 = d2/(j+1)/(j+2)
     end
     return(conv(a,b))
 end
@@ -45,10 +46,15 @@ function makeP(d)
     s = [j+taup for j = 0:length(d)-1]
     d2 = d2 .* s
     p = zeros(n)
-    for j = 1:n
-        p[j] = sum(d2) * (-1)^(j-1) / factorial(2*j-1)
-        d2 = d2 .* s
-        d2 = d2 .* s
+    for k = 0:n-1
+      for j = 0:L
+        c = 1
+        for m = 2*k+1:-1:1
+          c = c * (j+taup)/m
+        end
+        p[k+1] = p[k+1] + d[j+1] * c
+      end
+      p[k+1] = p[k+1] * (-1)^k
     end
     return(p)
 end
