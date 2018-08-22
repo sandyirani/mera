629b7c0e4be368b1b8a71503d66af2b29f43802d
sandyirani <31449386+sandyirani@users.noreply.github.com>
2018-08-22 07:52:06 -0700

    c

--- norm.jl
+++ norm.jl
@@ -1,1 +1,20 @@
-sadas
+tau = .5
+function makeD(L)
+    d = ones(L+1)
+    for j = 2:L+1
+      d[j] = d[j-1]*(L-j+2)*(L-j+2-tau)/(j-1)/(j-1+tau)
+    end
+    return(d)
+end
+
+function testPi()
+
+    n = 20
+    res = zeros(n)
+    for j = 1:n
+        d = makeD(j)
+        p = [(exp(im*2.5))^k for k = 1:length(d)]
+        res[j] = abs.(d'*p)
+    end
+    return(res)
+end
