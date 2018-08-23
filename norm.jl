5a1baf93bde2413f1a9e3f0ef37058168d4a4d6a
sandyirani <31449386+sandyirani@users.noreply.github.com>
2018-08-23 12:05:41 -0700

    Commit

--- norm.jl
+++ norm.jl
@@ -17,3 +17,18 @@ function testPi()
     end
     return(res)
 end
+
+function test2(L)
+  d = makeD(L)
+  p = [(-1)^(k-1) for k = 1:length(d)]
+  a = d .* p
+  h = Int8(floor((L+1)/2))
+  b = [a[j] + a[L+2-j] for j = h:-1:1]
+  c = zeros(length(b))
+  c[1] = b[1]
+  for j = 2:length(c)
+    c[j] = c[j-1] + b[j]
+  end
+  @show(sum(b))
+  return(c,b)
+end
